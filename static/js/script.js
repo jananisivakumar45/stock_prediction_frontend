ecefbe8f1811d41368d428ff60820dadd1321bfb
jananisivakumar45 <jananisivakumar005@gmail.com>
2024-11-12 08:13:43 +0530

    backend url updated

--- static/js/script.js
+++ static/js/script.js
@@ -41,7 +41,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         try {
-            const response = await fetch("http://localhost:5000/predict", {
+            const response = await fetch("https://stock-prediction-backend-k21j.onrender.com", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
